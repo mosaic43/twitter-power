94d6660eab4be160cf0a8f071087fa665a2ee3fc
gauraklein <gaurakklein@gmail.com>
2019-09-03 18:21:22 -0500

    promise all

--- tweets.js
+++ tweets.js
@@ -1,6 +1,10 @@
 let tweetContainer = $('#tweetContainer');
 let datePickerEl = $('#datePicker');
 
+var tweets2019 = []
+var tweets2018 = []
+var tweets2017 = []
+
 datePickerEl.change(findTweets) //listens for a change on the datepicker
        
 
@@ -88,36 +92,27 @@ function renderTweets (tweet) {
 
 function fetchTweets2017 () {
     fetch('http://localhost:5000/historicalTweets/condensed_2017.json')
-    .then(function (response) {
-        console.log(response.json())
-        return response.json()
-
-    })
+    .then(res=>res.clone().json())
     }
 
     function fetchTweets2018 () {
         fetch('http://localhost:5000/historicalTweets/condensed_2018.json')
-        .then(function (response) {
-            return response.json()
-        })
+        .then(res=>res.clone().json())
         }
-
+        
         function fetchTweets2019 () {
             fetch('http://localhost:5000/historicalTweets/condensed_2019.json')
-            .then(function (response) {
-                return response.json()
-
-            })
+            .then(res=>res.clone().json())
             }
 
 Promise.all([fetchTweets2017(), fetchTweets2018(), fetchTweets2019()]).then((resultsArr) => {
 
-    const tweets2017 = resultsArr[0];
-    const tweets2018 = resultsArr[1];
-    const tweets2019 = resultsArr[2];
+    tweets2017 = resultsArr[0];
+    tweets2018 = resultsArr[1];
+    tweets2019 = resultsArr[2];
 
     console.log(resultsArr)
-    console.log('This is 2017', JSON.parse(tweets2017))
+    console.log('This is 2017', tweets2017)
     console.log('This is 2018', tweets2018)
     console.log('This is 2019', tweets2019)
 
