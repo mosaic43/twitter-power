af713889e7032564d56f8b6aeb0055d31bab1c1e
gauraklein <gaurakklein@gmail.com>
2019-09-05 22:15:52 -0500

    All features are functioning except for left and right buttons, going to attempt a large merge in retrospect it might not have been a good idea to build such large features without submitting any pull requests

--- scripts/tweetsCalendar.js
+++ scripts/tweetsCalendar.js
@@ -4,46 +4,72 @@
 //Look into changing background color based on market Trend
 
 
-var eventArray = [] //empty array in which to load tweets
+var calendarEvents = [] //empty array in which to load tweets
 var calendarEl = document.getElementById('calendar')
 
 //renders Calendar on dom load
 document.addEventListener('DOMContentLoaded', function () {
   
   calendar.render()
-  addTrumpPic()
-  $(".fc-day-number").click(renderTweetsForSpecificDay);
 
-  console.log(calendarEl);
 
-  //*************** 
-  // EVENT LISTENER 
   calendarEl.addEventListener("click", function(e) {
-    console.log(e.target)
+    var target = $(e.target);
+    console.log(e.target);
+
+    if (target.hasClass('fc-day-number')) {
+
+      
+      findTweets(target.data('goto').date);
+
+    } else if (target.hasClass('fc-day')) {
+      findTweets(target.data('date'))
+    }
   })
-  //************* */
 })
 
-function renderTweetsForSpecificDay () {
-  var selectedDay = $('.fc-today').data('date')
-  console.log("I've been clicked")
-  console.log(selectedDay)
 
-  // console.log(tweets2019.find(selectedDay))
+function findTweets (date) {
+  const chosenDate = date
+  tweetContainer.empty() // clears out previous tweets
+  $('#renderedTweets').removeClass("hidden")
+  if (chosenDate.includes('2019')) {
+    // for 2019
 
+    tweets2019.map(function (tweet) {
+      if (tweet.date === chosenDate) {
+        tweetContainer.append(renderTweets(tweet))
+      }
+    })
+  } else if (chosenDate.includes('2018')) {
+    // for 2018
 
+    tweets2018.map(function (tweet) {
+      if (tweet.date === chosenDate) {
+        tweetContainer.append(renderTweets(tweet))
+      }
+    })
+  } else if (chosenDate.includes('2017')) {
+    // for 2017
 
-
-  
-  
+    tweets2017.map(function (tweet) {
+      if (tweet.date === chosenDate) {
+        tweetContainer.append(renderTweets(tweet))
+      }
+    })
+  } else {
+    tweetContainer.html('NO TWEETS AVAILABLE')
+  }
 }
 
+
+
 function pushTweets () {
-//Adds tweets to calendar
-pushTweetEvents(tweets2019);
-pushTweetEvents(tweets2018);
-pushTweetEvents(tweets2017);
-console.log(eventArray);
+//Adds tweet count to calendar
+pushTweetCount(tweets2019);
+pushTweetCount(tweets2018);
+pushTweetCount(tweets2017);
+// console.log(calendarEvents);
 // calendar.render();
 
 }
@@ -52,12 +78,6 @@ pushTweets(); //adds tweets to eventArray;
 
 
 
-
-function addTrumpPic () {
-
-  $('.fc-day').addClass('trumpPicture'); //adds trump picture, can use make a css class for good and bad to correlate with closing numbers
-
-}
 //Calendar Object
 var calendar = new FullCalendar.Calendar(calendarEl, {
   plugins: ['dayGrid', 'interaction'],
@@ -69,23 +89,103 @@ var calendar = new FullCalendar.Calendar(calendarEl, {
   navLinks: true, // can click day/week names to navigate views
   // editable: true,
   eventLimit: true, // allow "more" link when too many events
-  events: eventArray,
-  dateClick: tweetChecker,
-  dayClick: function(date, jsEvent, view) {
-
-    alert('Clicked on: ' + date.format());
-
-    alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
-
-    alert('Current view: ' + view.name);
-
-    // change the day's background color just for fun
-    $(this).css('background-color', 'red');
-
-  }
+  events: calendarEvents
+  // dateClick: tweetChecker
 })
 
+///USED TO GET TWEET EVENT COUNT TO SHOW UP
+function pushTweetCount (tweetArray) {
+let tweetCount = 1;
+let tweetDate = tweetArray[0]['date'];
 
+for(let i=0; i < tweetArray.length; i++) {
+  if (tweetDate === tweetArray[i].date) {
+    tweetCount++;
+  } else if (tweetDate !== tweetArray[i].date) {
+    calendarEvents.push( {
+      title: "Trump tweeted " + tweetCount + " times",
+      start: tweetDate
+    })
+    tweetDate = tweetArray[i].date
+    tweetCount = 1
+  }
+}
+}
+
+function renderTweets (tweet) {
+  return `
+    <div class="tile is-4 is-parent">
+
+        <div class="tile is-child tweet">
+
+            <div class="box">
+                <article class="media">
+
+                    <div class="media-left">
+                        <figure class="image is-64x64">
+                            <img src="img/trump-face.png" alt="Trump Face">
+                        </figure>
+                    </div>
+
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                            <strong>Donald J. Trump</strong> <small>@realDonaldTrump</small> <small>${moment(tweet.created_at).format('h:mm A')}</small>
+                            <br>
+                            ${tweet.text}
+                            </p>
+                        </div>
+
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>`
+}
+
+
+//UNUSED
+
+function renderTweetsForSpecificDay () {
+  var selectedDay = $('.fc-today').data('date')
+  console.log("I've been clicked")
+  console.log(selectedDay)
+
+  // console.log(tweets2019.find(selectedDay))
+ 
+}
+
+//UNUSED
+
+function addTrumpPic () {
+
+  $('.fc-day').addClass('trumpPicture'); //adds trump picture, can use make a css class for good and bad to correlate with closing numbers
+
+}
+
+//UNUSED
+
+// function to add tweet events to calendar
+function pushTweetEvents (tweetArray) {
+  //runs through array and turns each tweet into an event object then adds it to event array
+tweetArray.map(individualTweet => {
+  // var tweetCount = 0;
+  if (individualTweet !== tweetArray.length) {
+  calendarEvents.push( {
+    title: individualTweet.text,
+    start: individualTweet.date,
+  }, ) 
+  } else {
+    calendarEvents.push( {
+      title: 'Last Tweet',
+      start: individualTweet.date
+    } )
+  }
+
+})
+}
+
+//UNUSED
 
 // checks tweet and pulls proper object based on dateClick
 function tweetChecker (info) {
@@ -101,25 +201,3 @@ function tweetChecker (info) {
     console.log('NO TWEETS BITCH')
   }
 }
-
-// function to add tweet events to calendar
-function pushTweetEvents (tweetArray) {
-    //runs through array and turns each tweet into an event object then adds it to event array
-  tweetArray.map(individualTweet => {
-    if (individualTweet !== tweetArray.length) {
-    eventArray.push( {
-      title: individualTweet.text,
-      start: individualTweet.date,
-      imageurl: '../img/trump-face.png'
-    }, ) 
-    } else {
-      eventArray.push( {
-        title: 'Last Tweet',
-        start: individualTweet.date
-      } )
-    }
-
-  })
-}
-
-
