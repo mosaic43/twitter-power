a9646b5ed9613255cc7200c8af8fb8a3ad6205fb
Chantee Smith <chantee.smith@gmail.com>
2019-09-07 10:20:45 -0500

    default date change

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,22 +1,8 @@
 
-//document.addEventListener('DOMContentLoaded', function () {
-/////////////////Alphavantage Import////////////////////
-// var yesterday = moment().subtract(1, "days").format("YYYY-MM-DD");
 var keyOne = '95HH1JNCQL22W9NX'
 var keyTwo = 'W9RSPLFTK9VC6IAY'
 var keyThree = '5BFY5U7SB317S6H0'
-// var calendarEvents = []
-// var dowArray = []
-// var SP500Array = []
-// var NASDAQArray = []
-// var oilArray = []
-// var goldArray = []
-// var fullDowJSON = []
-// var fullSP500JSON = []
-// var fullNASDAQJSON = []
-// var fullOilJSON = []
-// var fullGoldJSON = []
-// var calendarDateClick = ""
+//var calendarEvents = []
 var symbolTicker
 var open = "1. open"
 var close = "4. close"
@@ -26,10 +12,6 @@ var spOil = []
 var spSpx = []
 var spGold = []
 var dowEvent = []
-
-//adding tweet count to events
-
-
 
 //////// Main Function for Creating Event for Calendar////////
 function getEvents(tickerData) {
@@ -129,7 +111,7 @@ function setCalendar() {
       center: 'title,addEventButton',
       right: 'dayGridMonth,dayGridWeek,dayGridDay'
     },
-    defaultDate: '2019-08-12',
+    defaultDate: '2019-09-07',
     navLinks: true, // can click day/week names to navigate views
     editable: true,
     eventLimit: true, // allow "more" link when too many events
